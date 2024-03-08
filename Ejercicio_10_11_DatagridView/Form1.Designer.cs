8716f7b84c1fd2ea39643e301a860590b31a5f12
pc <pc@LENNY>
2024-03-08 12:42:20 -0400

    Agregar archivos de proyecto.

---
+++ Ejercicio_10_11_DatagridView/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Ejercicio_10_11_DatagridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.lbl_nombre = new System.Windows.Forms.Label();
+            this.dgv_nombre_correo = new System.Windows.Forms.DataGridView();
+            this.btn_Ingresar = new System.Windows.Forms.Button();
+            this.lbl_email = new System.Windows.Forms.Label();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_cerrar = new System.Windows.Forms.Button();
+            this.lb_Titulo = new System.Windows.Forms.Label();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.E_mail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_nombre_correo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(161, 105);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(100, 20);
+            this.txt_nombre.TabIndex = 0;
+            // 
+            // lbl_nombre
+            // 
+            this.lbl_nombre.AutoSize = true;
+            this.lbl_nombre.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nombre.Location = new System.Drawing.Point(185, 89);
+            this.lbl_nombre.Name = "lbl_nombre";
+            this.lbl_nombre.Size = new System.Drawing.Size(48, 14);
+            this.lbl_nombre.TabIndex = 1;
+            this.lbl_nombre.Text = "Nombre";
+            // 
+            // dgv_nombre_correo
+            // 
+            this.dgv_nombre_correo.BackgroundColor = System.Drawing.Color.Teal;
+            this.dgv_nombre_correo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_nombre_correo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.E_mail});
+            this.dgv_nombre_correo.Location = new System.Drawing.Point(177, 175);
+            this.dgv_nombre_correo.Name = "dgv_nombre_correo";
+            this.dgv_nombre_correo.Size = new System.Drawing.Size(341, 150);
+            this.dgv_nombre_correo.TabIndex = 3;
+            // 
+            // btn_Ingresar
+            // 
+            this.btn_Ingresar.BackColor = System.Drawing.Color.Cyan;
+            this.btn_Ingresar.Location = new System.Drawing.Point(579, 185);
+            this.btn_Ingresar.Name = "btn_Ingresar";
+            this.btn_Ingresar.Size = new System.Drawing.Size(96, 25);
+            this.btn_Ingresar.TabIndex = 4;
+            this.btn_Ingresar.Text = "Ingresar";
+            this.btn_Ingresar.UseVisualStyleBackColor = false;
+            this.btn_Ingresar.Click += new System.EventHandler(this.btn_Ingresar_Click);
+            // 
+            // lbl_email
+            // 
+            this.lbl_email.AutoSize = true;
+            this.lbl_email.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_email.Location = new System.Drawing.Point(431, 89);
+            this.lbl_email.Name = "lbl_email";
+            this.lbl_email.Size = new System.Drawing.Size(38, 14);
+            this.lbl_email.TabIndex = 5;
+            this.lbl_email.Text = "E-mail";
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(396, 105);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(100, 20);
+            this.txt_email.TabIndex = 6;
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.BackColor = System.Drawing.Color.Cyan;
+            this.btn_eliminar.Location = new System.Drawing.Point(579, 214);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(96, 25);
+            this.btn_eliminar.TabIndex = 7;
+            this.btn_eliminar.Text = "Eliminar";
+            this.btn_eliminar.UseVisualStyleBackColor = false;
+            this.btn_eliminar.Click += new System.EventHandler(this.btn_eliminar_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.BackColor = System.Drawing.Color.Cyan;
+            this.btn_limpiar.Location = new System.Drawing.Point(579, 243);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(96, 25);
+            this.btn_limpiar.TabIndex = 8;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = false;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // btn_cerrar
+            // 
+            this.btn_cerrar.BackColor = System.Drawing.Color.Cyan;
+            this.btn_cerrar.Location = new System.Drawing.Point(579, 272);
+            this.btn_cerrar.Name = "btn_cerrar";
+            this.btn_cerrar.Size = new System.Drawing.Size(96, 25);
+            this.btn_cerrar.TabIndex = 9;
+            this.btn_cerrar.Text = "Cerrar";
+            this.btn_cerrar.UseVisualStyleBackColor = false;
+            this.btn_cerrar.Click += new System.EventHandler(this.btn_cerrar_Click);
+            // 
+            // lb_Titulo
+            // 
+            this.lb_Titulo.AutoSize = true;
+            this.lb_Titulo.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Titulo.Location = new System.Drawing.Point(268, 31);
+            this.lb_Titulo.Name = "lb_Titulo";
+            this.lb_Titulo.Size = new System.Drawing.Size(138, 21);
+            this.lb_Titulo.TabIndex = 10;
+            this.lb_Titulo.Text = "Ingreso de Datos";
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // E_mail
+            // 
+            this.E_mail.FillWeight = 200F;
+            this.E_mail.HeaderText = "E-mail";
+            this.E_mail.Name = "E_mail";
+            this.E_mail.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(729, 384);
+            this.Controls.Add(this.lb_Titulo);
+            this.Controls.Add(this.btn_cerrar);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_eliminar);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.lbl_email);
+            this.Controls.Add(this.btn_Ingresar);
+            this.Controls.Add(this.dgv_nombre_correo);
+            this.Controls.Add(this.lbl_nombre);
+            this.Controls.Add(this.txt_nombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_nombre_correo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.Label lbl_nombre;
+        private System.Windows.Forms.DataGridView dgv_nombre_correo;
+        private System.Windows.Forms.Button btn_Ingresar;
+        private System.Windows.Forms.Label lbl_email;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.Button btn_eliminar;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_cerrar;
+        private System.Windows.Forms.Label lb_Titulo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn E_mail;
+    }
+}
+
---
+++ Ejercicio_10_11_DatagridView/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_10_11_DatagridView
+{
+    public partial class Form1 : Form
+    {
+        string nombre;
+        string correo;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Ingresar_Click(object sender, EventArgs e)
+        {
+
+            nombre = txt_nombre.Text;
+            correo = txt_email.Text;
+
+            if (txt_nombre.Text == "" || txt_email.Text == "")
+            {
+                MessageBox.Show("Por favor, rellene todos los campos", "Campos vacíos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                dgv_nombre_correo.Rows.Add(nombre, correo);
+
+                txt_nombre.Clear();
+                txt_email.Clear();
+            }
+
+            txt_nombre.Focus();
+
+
+        }
+
+        private void btn_eliminar_Click(object sender, EventArgs e)
+        {
+
+            if (dgv_nombre_correo.SelectedRows.Count <= 0)
+            {
+                MessageBox.Show("No hay datos para eliminar", "Tabla vacía", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+
+          
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txt_nombre.Clear();
+            txt_email.Clear();
+
+            dgv_nombre_correo.Rows.Clear();
+            txt_nombre.Focus();
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            txt_nombre.Clear();
+            txt_email.Clear();
+
+            dgv_nombre_correo.Rows.Clear();
+        }
+
+        private void btn_cerrar_Click(object sender, EventArgs e)
+        {
+            DialogResult respuesta = MessageBox.Show("¿Desea salir de la aplicación?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (respuesta == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+    }
+
